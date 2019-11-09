da64affefac65ca26e7d00ec474cb97f3fffb032
truongloc <nmtruongloc806@gmail.com>
2019-11-09 15:40:33 +0700

    add new project

---
+++ GoogleMapsDemo/GoogleMapsDemo/Customs/MapCustom.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace GoogleMapsDemo.Customs
+{
+    public class MapCustom : Map
+    {
+    }
+}
