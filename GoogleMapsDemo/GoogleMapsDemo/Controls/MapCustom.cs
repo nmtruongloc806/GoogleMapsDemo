266cb82f13c1f24e3709c6a6b6fa95a85fc391ab
truongloc <nmtruongloc806@gmail.com>
2019-11-09 16:51:06 +0700

    thay doi ten customs thanh controls

--- GoogleMapsDemo/GoogleMapsDemo/Controls/MapCustom.cs
+++ GoogleMapsDemo/GoogleMapsDemo/Controls/MapCustom.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms.Maps;
 
-namespace GoogleMapsDemo.Customs
+namespace GoogleMapsDemo.Controls
 {
     public class MapCustom : Map
     {
