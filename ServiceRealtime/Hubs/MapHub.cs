5555ec9ab41cbe9ae46c6590566a35bdaf96cf3b
truongloc <nmtruongloc806@gmail.com>
2019-11-11 14:14:08 +0700

    add signalr service

---
+++ ServiceRealtime/Hubs/MapHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRService.Hubs
+{
+    public class MapHub : Hub
+    {
+        public async Task SendLocation(string iconColor, float lat, float lng)
+        {
+            await Clients.All.SendAsync("ReceiveLocation", iconColor, lat, lng);
+        }
+    }
+}
